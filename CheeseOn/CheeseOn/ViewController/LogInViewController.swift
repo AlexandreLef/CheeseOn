de3ef77c01b3f69a270c5471b261293508ac1067
AlexandreLef <alex.lfebvre@gmail.com>
2021-11-18 15:30:36 +0100

    [CheeseOn][Login][Creation] W.I.P.

--- CheeseOn/CheeseOn/ViewController/LogInViewController.swift
+++ CheeseOn/CheeseOn/ViewController/LogInViewController.swift
@@ -8,25 +8,49 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var keepConnect: UIButton!
+    @IBOutlet weak var connexion: UIButton!
 
+    var keepConnectStatus: Bool = false
+    var style = Style()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(LogInViewController.home)) // Save the tap on button
+        connexion.addGestureRecognizer(gesture) // Cast the gesture
+        
+        
+        connexion.layer.borderWidth = 7
+        connexion.layer.borderColor = style.purple.cgColor
+        connexion.layer.cornerRadius = connexion.frame.size.height / 2 
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated) // Remove the navigation bar on the view
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+    @objc func home() {     // Change page to Home page
+        let storyboard = UIStoryboard(name: "Main" , bundle: nil)
+        let homeViewController = storyboard.instantiateViewController(withIdentifier:"homeViewController")as! HomeViewController
+        self.navigationController?.pushViewController(homeViewController, animated:true) // Push the Home page
     }
-    */
+    
+    @IBAction func connect() {
+        let user = User(userName: username.text!, keepConneced: keepConnectStatus)
+        user.registerUser()
+    }
 
+    @IBAction func checkKeepConnect() {
+        keepConnectStatus = !keepConnectStatus
+        if keepConnectStatus {
+            keepConnect.setImage(UIImage(systemName: "record.circle.fill"), for: .normal)
+        } else {
+            keepConnect.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+        }
+    }
+    
 }
