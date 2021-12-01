991c9075650cf1bd6f30b77211d4bda76c8f1d48
AlexandreLef <alex.lfebvre@gmail.com>
2021-12-01 08:25:13 +0100

    [CheeseOn][Android][Correction]

--- CheeseOn_Android/app/src/main/java/com/example/cheeseon/MainActivity.java
+++ CheeseOn_Android/app/src/main/java/com/example/cheeseon/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.cheeseon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.cheeseon.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View v = mBinding.getRoot();
+
+        mBinding.btnConnexion.isClickable();
+
+        setContentView(v);
     }
+
 }
